0a715aff2d264bea91c4425aa74ef7fc54fc976f
unknown <Joseph.Kampa@gmail.com>
2018-07-26 16:17:34 -0500

    Fixed Click Tile, Header Components

--- src/components/AlbumTile/AlbumTile.js
+++ src/components/AlbumTile/AlbumTile.js
@@ -1,4 +1,12 @@
 import React from "react";
+import "./AlbumTile.css";
 
+const AlbumTile = props => (
+  <div className="albumTile" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
 
-export default playingCard;
+export default AlbumTile;
