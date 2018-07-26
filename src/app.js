005e0db189d035a45b44a73de3ce2d2aea7de7e0
unknown <Joseph.Kampa@gmail.com>
2018-07-26 16:00:38 -0500

    Added 3 fields of logic to app.js, added count field to albums.json

--- src/app.js
+++ src/app.js
@@ -2,20 +2,65 @@ import React, { Component } from "react";
 import AlbumTile from "./components/AlbumTile";
 import Body from "./components/Body";
 import Header from "./components/Header";
-import Albums from "./albums.json";
+import albums from "./albums.json";
 
 class App extends Component {
     state = {
         albums,
         score: 0,
-        highscore
+        highscore: 0
     };
 
-    //endgame
-
-
     //scoring
+    clickCount = id => {
+        this.state.albums.find((h, i) => {
+            if (h.id === id) {
+                if (albums[i].count === 0) {
+                    albums[i].count = albums[i].count + 1;
+                    this.setState({ score: this.state.score + 1 }, function () {
+                        console.log(this.state.score);
+                    });
+                    this.state.albums.sort(() => Math.random() - 0.5)
+                    return true;
+                } else {
+                    this.endGame();
+                }
+            }
+        });
+    }
 
 
-    //render
+    //endgame
+    endGame = () => {
+        if (this.state.score > this.state.highscore) {
+            this.setState({ highscore: this.state.highscore }, function () {
+                console.log(this.state.highscore);
+            });
+        }
+        this.state.albums.forEach(albumArt => {
+            albumArt.count = 0;
+        });
+        alert(`Album Click Game...OVER! The Final Score:\n ${this.state.score}`);
+        this.setState({ score: 0 });
+        return true;
+    }
+
+    //render albums
+    render() {
+        return (
+            <Body>
+                <Header score={this.state.score} highscore={this.state.highscore}>Album Click Game</Header>
+                {this.state.albums.map(albumArt => (
+                    <AlbumTile
+                        clickCount={this.clickCount}
+                        id={albumArt.id}
+                        key={albumArt.id}
+                        image={albumArt.image}
+                    />
+                ))}
+            </Body>
+        );
+    }
 }
+
+export default App;
